e320604c3bf34e2531071686b15379f0fd1d3067
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-11-19 19:00:13 +0100

    complete train script

--- train.py
+++ train.py
@@ -1,34 +1,26 @@
-from matplotlib import pyplot as plt
-import numpy as np
-import os
-import cv2 
-import csv
 import tensorflow as tf
-from keras.preprocessing import image
-from keras.models import Model, load_model
-from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
 from keras.applications.xception import Xception
 from keras.applications.densenet import DenseNet121
 from keras.applications.inception_v3 import InceptionV3
 
-from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten
-import math
-from PIL import Image
 
 from keras.optimizers import gradient_descent_v2
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import ModelCheckpoint
 
 classes_path = 'The path to the txt file for the classes'
-train_dataset = 'The path to the train datasets'
+dataset_path = 'The path to the train datasets'
 input_dim = 224
+epochs = 50
 
-def return_classes():
+def return_classes(classes_path):
     with open(classes_path, 'r') as f:
         classes = f.readlines()
         classes = list(map(lambda x: x.strip(), classes))
-
-#num_classes = len(classes)
+    
+    return classes
 
 def return_ds(input_dir):
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -50,10 +42,9 @@ def return_ds(input_dir):
         batch_size=8, 
     label_mode='categorical',
     )
-
     return train_ds, val_ds
 
-def return_model(input_dim, nb_classes, head=None):
+def return_model(input_dim, nb_classes, freeze=False, head=None):
     if head == 'xception' or head == 'Xception':
         base_model = Xception(include_top=False, weights='imagenet', input_shape=(input_dim, input_dim, 3))
     
@@ -69,6 +60,10 @@ def return_model(input_dim, nb_classes, head=None):
     if not head:
         print('Please choose the pretrained model')
 
+    if freeze:
+        for layer in base_model.layers:
+            layer.trainable = False
+
 
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
@@ -77,5 +72,14 @@ def return_model(input_dim, nb_classes, head=None):
     predictions = Dense(nb_classes, activation='softmax')(x)
     model = Model(inputs=base_model.inputs, outputs=predictions)
 
+    return model
 
-return_model(244, 2)
+classes = return_classes(classes_path)
+train_ds, val_ds = return_ds(dataset_path)
+model = return_model(input_dim, len(classes))
+
+model.compile(loss='categorical_crossentropy', optimizer=gradient_descent_v2.SGD(learning_rate=0.01), metrics=['accuracy'])
+save_weights = ModelCheckpoint(filepath='models/my_model.h5', monitor='val_accuracy', 
+                                verbose=1, save_best_only=True, save_weights_only=False, mode='max')
+
+model.fit(train_ds, epochs=epochs, validation_data=val_ds, callbacks=[save_weights])
