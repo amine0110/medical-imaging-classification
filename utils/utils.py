a0957f9a4b348628da931472771b97f12ad3c9c5
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-11-19 21:15:15 +0100

    added utils.py for additional functions

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+from glob import glob
+import os
+
+def return_classes(classes_path):
+    with open(classes_path, 'r') as f:
+        classes = f.readlines()
+        classes = list(map(lambda x: x.strip(), classes))
+    
+    return classes
+
+def create_classes(path_datasets):
+    paths = glob(path_datasets + '/*')
+    file = open('annotation.txt', 'w')
+    for path in paths:
+        file.write(os.path.basename(path) + '\n')
