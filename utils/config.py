0e8317ca9ebfc5f2cde25e880555b7c5dd1ea1a8
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-11-19 21:14:45 +0100

    config file updated

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,10 @@
+# Data paths
 classes_path = 'utils/annotation.txt'
-dataset_path = './dataset'
+train_dataset_path = './dataset/train'
+test_dataset_path = './dataset/test'
+
+# Training configs
 input_dim = 299
-epochs = 50
+epochs = 20
+lr=0.01
+head = 'xception'
